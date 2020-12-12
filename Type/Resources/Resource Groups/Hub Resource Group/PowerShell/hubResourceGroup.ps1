b16c44e2a62ec1576905fd2498bddf76ae76bc65
Henry Gelderbloem <hgelderbloem@icloud.com>
2020-12-12 16:36:56 +0000

    Initial version of Hub Resource Group in PowerShell

---
+++ Type/Resources/Resource Groups/Hub Resource Group/PowerShell/hubResourceGroup.ps1
@@ -0,0 +1,28 @@
+# ----------------------------------------------------------------------------------------------
+# --- ABOUT
+
+# CREATED BY: Henry Gelderbloem
+# VERSION: 1.0
+
+# ----------------------------------------------------------------------------------------------
+# --- Variables
+
+# Resource Group Location
+$rgLocation = Read-Host -Prompt "Enter the Resource Group location"
+
+# Resource Group Name
+$rgName = "hub-prod-$rgLocation-rg-01"
+
+# ----------------------------------------------------------------------------------------------
+# --- Create Hub Resource Group
+
+# Create Hub Resource Group
+New-AzResourceGroup -Name $rgName -Location $rgLocation
+
+# Confirm creation of Hub Resource Group
+do {
+    $rgProvisioningState = Get-AzResourceGroup -Name $rgName -ErrorAction Ignore | ForEach-Object {$_.ProvisioningState -eq  'succeeded'}
+} until ($rgProvisioningState -eq 'True')
+
+# Create Resource Group cannot delete lock
+New-AzResourceLock -ResourceGroupName $rgName -LockLevel CanNotDelete -Name "Cannot delete $rgName" -LockNotes "Protects $rgName from deletion." -Force
